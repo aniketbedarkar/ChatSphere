7452763063ed064def8976dea5f7d09402c65302
aniketbedarkar <aniketbedarkar1962@gmail.com>
2024-08-28 10:05:50 +0530

    Updated and working

--- src/main/java/com/nest/chatsphere/Controller/ChatController.java
+++ src/main/java/com/nest/chatsphere/Controller/ChatController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Slf4j
 @Controller
+@CrossOrigin("**")
 public class ChatController {
 
     @Autowired
