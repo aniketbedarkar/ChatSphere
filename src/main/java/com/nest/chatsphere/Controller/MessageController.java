79bf6280dcc4b57ce262b2ce638cffcafc73a0c7
aniketbedarkar <aniketbedarkar1962@gmail.com>
2024-09-06 10:02:47 +0530

    Added /deleteAll to delete all the messages. And improved the no messages string.

--- src/main/java/com/nest/chatsphere/Controller/MessageController.java
+++ src/main/java/com/nest/chatsphere/Controller/MessageController.java
@@ -5,10 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -42,4 +39,9 @@ public class MessageController {
         return ResponseEntity.ok(Collections.singletonMap("hashcode",String.valueOf(messageService.getHashCode(request))));
     }
 
+    @DeleteMapping("/allMessages")
+    public ResponseEntity<?> deleteAllMessages(){
+        messageService.deleteAllMesages();
+        return ResponseEntity.ok("Successfully deleted all messages.");
+    }
 }
--- src/main/java/com/nest/chatsphere/Service/MessageService.java
+++ src/main/java/com/nest/chatsphere/Service/MessageService.java
@@ -1,7 +1,9 @@
 package com.nest.chatsphere.Service;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -12,19 +14,31 @@ import java.util.*;
 @Service
 @Slf4j
 public class MessageService {
-    private final List<Map<String,String>> messages = new ArrayList<>();
 
-    public List<Map<String,String>> getMessages(){
-        return this.messages;
-    }
+    @Value("${code.delete.all}")
+    private String deleteCode;
+
+    @Getter
+    private List<Map<String,String>> messages = new ArrayList<>();
 
     public void addMessages(String message, int requestHash) {
-        Map<String,String> messageMap = new HashMap<>();
-        messageMap.put("text",message);
-        messageMap.put("timeStamp", getCurrentTimestamp());
-        messageMap.put("requestHash", String.valueOf(requestHash));
-        this.messages.add(messageMap);
+        if(message.startsWith("/")){
+            codedMessage(message);
+        }else {
+            Map<String, String> messageMap = new HashMap<>();
+            messageMap.put("text", message);
+            messageMap.put("timeStamp", getCurrentTimestamp());
+            messageMap.put("requestHash", String.valueOf(requestHash));
+            this.messages.add(messageMap);
+        }
     }
+
+    private void codedMessage(String code) {
+        if(code.equals(deleteCode)){
+            deleteAllMesages();
+        }
+    }
+
     public static String getCurrentTimestamp() {
     	log.debug("Inside getCurrentTimeStamp");
         Instant now = Instant.now();
@@ -38,6 +52,7 @@ public class MessageService {
         Map<String,String> noMessageMap = new HashMap<>();
         noMessageMap.put("timeStamp",getCurrentTimestamp());
         noMessageMap.put("text","No messages yet.");
+        noMessageMap.put("requestHash",String.valueOf("Chatbox".hashCode()));
         return Collections.singletonList(noMessageMap);
     }
 
@@ -45,5 +60,10 @@ public class MessageService {
         int hashcode = (request.getHeader("User-Agent")+request.getRemoteAddr()).hashCode();
 //        log.info("Getting hashcode: "+hashcode);
         return hashcode;
+    }
+
+    public boolean deleteAllMesages() {
+        messages = new ArrayList<>();
+        return true;
     }
 }
